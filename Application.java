a9aa7fe39936107e20df1559899b40d50cce6c96
Hannah <hannahibkekundayo@gmail.com>
2024-11-01 13:15:10 +0100

    Instantiate calculator.java file and also added .gitignore file to our project

--- Application.java
+++ Application.java
@@ -1,7 +1,16 @@
 public class Application{
  public static void main(String... args){
+	calculator cal = new calculator();
 
-
+	system.out.println(cal.addition(5,6));
+	// should be 11
+	
+	System.out.println(cal.addition(6,3.5));
+	// should be 9.5
+	
+	System.out.println(cal.subtraction(5.5,3.5));
+	// should be 2.0
+	
+	
  }
-
 }
